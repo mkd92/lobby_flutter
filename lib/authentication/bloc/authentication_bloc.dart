7acfd2a61df0e6a790724f77dae9242a9cb7e850
mkd92 <manikandan.m1116@gmail.com>
2021-05-17 21:03:17 +0530

    bloc app implementation

---
+++ lib/authentication/bloc/authentication_bloc.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+import 'dart:html';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../user_repository.dart';
+import '../../authentication_repository.dart';
+
+part 'authentication_event.dart';
+part 'authentication_state.dart';
+
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  AuthenticationBloc({
+    required AuthenticationRepository authenticationRepository,
+    required UserRepository userRepository,
+  })   : _authenticationRepository = authenticationRepository,
+        _userRepository = userRepository,
+        super(const AuthenticationState.unknown()) {
+    _authenticationStatusSubscription = _authenticationRepository.status
+        .listen((status) => add(AuthenticationStatusChanged(status)));
+  }
+  final AuthenticationRepository _authenticationRepository;
+  final UserRepository _userRepository;
+  late StreamSubscription<AuthenticationStatus>
+      _authenticationStatusSubscription;
+
+  @override
+  Stream<AuthenticationState> mapEventToState(
+    AuthenticationEvent event,
+  ) async* {
+    if (event is AuthenticationStatusChanged) {
+      yield await _mapAuthenticationStatusChangedToState(event);
+    } else if (event is AuthenticationLogoutRequested) {
+      _authenticationRepository.logOut();
+    }
+  }
+
+  @override
+  Future<void> close() {
+    _authenticationStatusSubscription.cancel();
+    _authenticationRepository.dispose();
+    return super.close();
+  }
+  //TODO add _mapAuthenticationStatusChangedToState
+}
--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'bloc/blocs.dart';
-import 'services/services.dart';
-import 'pages/pages.dart';
